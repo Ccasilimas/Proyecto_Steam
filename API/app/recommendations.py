83cbf08a51dfd59a51a3e9660a5965cc0615594f
Camilo Casilimas <40143182+Ccasilimas@users.noreply.github.com>
2024-10-14 22:04:49 -0500

    .

--- API/app/recommendations.py
+++ API/app/recommendations.py
@@ -5,9 +5,9 @@ from sklearn.decomposition import PCA
 import gzip
 
 # Cargar los datos y descomprimirlos
-df_games = pd.read_csv('/app/Datos/games.csv.gz', compression='gzip') # API\app\Datos\games.csv.gz
-df_users = pd.read_csv('/app/Datos/Items.csv.gz', compression='gzip')  
-df_reviews = pd.read_csv('/app/Datos/reviews.csv.gz', compression='gzip') 
+df_games = pd.read_csv('/opt/render/project/src/API/app/Datos/games.csv.gz', compression='gzip') # API\app\Datos\games.csv.gz
+df_users = pd.read_csv('/opt/render/project/src/API/app/Datos/Items.csv.gz', compression='gzip')  
+df_reviews = pd.read_csv('/opt/render/project/src/API/app/Datos/reviews.csv.gz', compression='gzip') 
 
 # Calcular TF-IDF
 tfidf_vectorizer = TfidfVectorizer()
